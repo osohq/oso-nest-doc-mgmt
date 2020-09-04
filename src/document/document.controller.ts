bbca136f8e69e796538229065702a406a48daac7
orion <oletizi@mac.com>
2020-09-04 12:45:58 -0700

    - Incrementally adding CRUD implementation for documents and comments;

--- src/document/document.controller.ts
+++ src/document/document.controller.ts
@@ -1,18 +1,32 @@
-import { Controller, Param, Get, Req, UseGuards } from '@nestjs/common';
-import { DocumentService } from './document.service';
+import { Controller, Param, Get, Req, UseGuards, Post, Body } from '@nestjs/common';
+import { CreateDocumentDto } from './dto/document.dto';
+import { Document, DocumentService } from './document.service';
 import { OsoInstance, Authorize } from '../oso/oso.guard';
 import { LocalAuthGuard } from '../auth/local-auth.guard';
 
-@UseGuards(LocalAuthGuard)
-@UseGuards(OsoInstance)
+
+//@UseGuards(OsoInstance)
 @Controller('document')
 export class DocumentController {
-  constructor(private readonly documentService: DocumentService) {}
+  constructor(private readonly documentService: DocumentService) {
+  }
 
+  @UseGuards(LocalAuthGuard)
   @Get(':id')
   async findOne(@Param() param, @Authorize() authorize): Promise<string> {
     const document = await this.documentService.findOne(Number.parseInt(param.id));
     await authorize(document);
     return document.document;
+  }
+
+  @Get()
+  async findAll(): Promise<Document[]> {
+    return await this.documentService.findAll()
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('create')
+  async create(@Body() document: CreateDocumentDto): Promise<number> {
+    return this.documentService.create(document.basId, document.document)
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getLogger } from 'log4js'
+
+getLogger().level = 'info'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
 bootstrap();
