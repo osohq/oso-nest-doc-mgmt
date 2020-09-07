a66b3b117a55f000ede4e5d54df8bb1cb26577d4
orion <oletizi@mac.com>
2020-09-07 14:36:21 -0700

    - updated DocumentController to use OsoGuard instead of OsoInstance - updated Action to set metadata properly - updated OsoModule metadata - put log4js back b/c it's more usable than the nest logger.

--- src/document/document.controller.ts
+++ src/document/document.controller.ts
@@ -1,17 +1,17 @@
 import { Controller, Param, Get, UseGuards, Post, Body } from '@nestjs/common';
-import { OsoInstance } from '../oso/oso-instance';
 import { CreateDocumentDto } from './dto/document.dto';
 import { DocumentService } from './document.service';
-import { Authorize } from '../oso/oso.guard';
+import { Action, Authorize, OsoGuard } from '../oso/oso.guard';
 import { LocalAuthGuard } from '../auth/local-auth.guard';
 
-@UseGuards(OsoInstance)
+@UseGuards(OsoGuard)
 @UseGuards(LocalAuthGuard)
 @Controller('document')
 export class DocumentController {
   constructor(private readonly documentService: DocumentService) {
   }
 
+  @Action('read')
   @Get(':id')
   async findOne(@Param() param: any, @Authorize() authorize: any): Promise<string> {
     const document = await this.documentService.findOne(Number.parseInt(param.id));
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
-import { NestApplicationOptions } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const shouldLog = false;
-  const app = await NestFactory.create(AppModule, {
-    logger: shouldLog ? ['log', 'error', 'warn'] : false
-  });
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 
