8592afa1ae071317af03fe6878e06306aacf58d7
orion <oletizi@mac.com>
2020-09-17 15:42:05 -0700

    - added capability to add new members to a project in ProjectService

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
 export class Project {
+  private readonly members: Set<number> = new Set();
+
   constructor(public readonly id: number, public readonly ownerId: number) {
+    this.addMember(ownerId);
+  }
+
+  addMember(userId: number): void {
+    this.members.add(userId);
+  }
+
+  getMembers(): Set<number> {
+    return new Set(this.members);
   }
 }
 
@@ -18,5 +29,13 @@ export class ProjectService {
 
   findOne(id: number): Project {
     return this.projects[id];
+  }
+
+  addMember(id: number, userId: number): void {
+    this.findOne(id).addMember(userId);
+  }
+
+  findMembers(projectId: number): Set<number> {
+    return this.findOne(projectId).getMembers();
   }
 }
