6ed9edecd9e372c750c92904ebfb715042feec77
Sam Scott <sam@osohq.com>
2020-10-10 11:20:41 -0400

    No longer need the interceptor.

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,12 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { getLogger } from 'log4js';
 import { AppModule } from './app.module';
-import { UsersInterceptor } from './users/users-interceptor';
 
 // set the global logging level for log4js
 getLogger().level = 'warn';
 
-export async function configApp(app: INestApplication) {
-  app.useGlobalPipes(new ValidationPipe()).useGlobalInterceptors(new UsersInterceptor());
-}
-
 async function bootstrap() {
-  const app: INestApplication = await NestFactory.create(AppModule);
-  await configApp(app);
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-
 bootstrap();
